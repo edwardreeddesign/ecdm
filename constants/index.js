385c5e09ba4bc292ee66fa02bf437c92ad3f2f9b
Ed Reed <edreed@Eds-MacBook-Air.local>
2024-11-03 10:47:42 -0500

    removed football event

--- constants/index.js
+++ constants/index.js
@@ -394,13 +394,13 @@ export const upcomingEvents = [
   //   date: 'Starting January 8th, 2024',
   //   location: '68 MURRAY ST. AMHERSTBURG',
   // },
-  {
-    name: 'Football Game And Tailgate Party ',
-    imageUrl: '/images/events/footballPoster.pdf',
-    description: 'UofM vs Northwestern raffle. Tickets are $20.00 ',
-    date: 'Saturday November 23rd, 2024',
-    location: 'Draw date November 15th, 2024',
-  },
+  // {
+  //   name: 'Football Game And Tailgate Party ',
+  //   imageUrl: '/images/events/footballPoster.pdf',
+  //   description: 'UofM vs Northwestern raffle. Tickets are $20.00 ',
+  //   date: 'Saturday November 23rd, 2024',
+  //   location: 'Draw date November 15th, 2024',
+  // },
 
   {
     name: 'Widows Sons South Detroit ',
